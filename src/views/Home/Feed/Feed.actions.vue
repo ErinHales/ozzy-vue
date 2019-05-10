1a025c148e70b85969904d69f1ad312f9227dce2
Erin Hales <erin@summa.one>
2019-05-10 14:30:52 -0600

    adding more to post component

---
+++ src/views/Home/Feed/Feed.actions.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="actions">
+    <v-layout>
+      <div class="actions__icon">
+        <Heart></Heart>
+      </div>
+      <p>{{ numLikes }}</p>
+    </v-layout>
+    <v-text-field outline color="deepBlue" placeholder="Comment. . ."></v-text-field>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Actions',
+
+  data: () => ({
+    numLikes: 10
+  }),
+
+  components: {
+    Heart: () => import('@/components/app/Heart')
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+.actions
+  &__icon
+    width 30px
+    height 30px
+    margin 10px
+</style>
--- src/views/Home/Feed/Feed.post.vue
+++ src/views/Home/Feed/Feed.post.vue
@@ -4,8 +4,15 @@
       <v-card-title primary-title>
         <v-layout justify-space-between>
           <div>
-            <h3 class="headline mb-0">{{ post.author }}</h3>
-            <div>{{ post.date }}</div>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+              aspect-ratio="1"
+              class="post__header__img"
+            ></v-img>
+            <div class="post__header__post-info">
+              <h3 class="headline mb-0">{{ post.author }}</h3>
+              <div>{{ post.date }}</div>
+            </div>
           </div>
           <div>
             <h3 class="post__category">{{ post.category }}</h3>
@@ -22,6 +29,9 @@
         src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
         aspect-ratio="2"
       ></v-img>
+      <v-card-actions>
+        <Actions class="post__actions"></Actions>
+      </v-card-actions>
     </v-card>
   </div>
 </template>
@@ -29,6 +39,11 @@
 <script>
 export default {
   name: 'Post',
+
+  components: {
+    Actions: () => import('./Feed.actions.vue')
+  },
+
   props: {
     post: {
       type: Object,
@@ -47,8 +62,17 @@ export default {
 <style scoped lang="stylus">
 .post
   width 100vw
-  max-width 800px
+  max-width 700px
   margin 20px 0
+  &__header
+    &__img
+      display inline-block
+      width 60px
+    &__post-info
+      display inline-block
+      position relative
+      bottom 10px
+      margin-left 10px
   &__category
     display inline-block
     position relative
@@ -59,4 +83,6 @@ export default {
     width 20px
     height 20px
     background-color pink
+  &__actions
+    width 100%
 </style>
